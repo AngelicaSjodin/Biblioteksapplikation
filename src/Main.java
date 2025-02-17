d82a593d235c01cead86099e7120b3a05df47af3
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-02-17 20:43:07 +0100

    rearranged files

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main{
+    public static void main (String[] args){
+        String url = "jdbc:mysql://localhost:3306/biblotekdatabase";
+        String user = "root";
+        String password = "Angelica123!";
+
+        try{
+            Connection conn = DriverManager.getConnection(url,user,password);
+            System.out.println("connected yay!");
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
